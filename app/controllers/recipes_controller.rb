68b39d8d79279e846220a1b3b1479cbb32781f11
github username <github email address>
2018-09-15 16:19:07 -0600

    added route to /recipes page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,12 @@
 class RecipesController < ApplicationController
 
-  
+  get '/recipes' do
+    if logged_in?
+      @user = current_user
+      erb :'/recipes/recipes'
+    else
+      redirect "/login"
+    end
+  end
 
 end
