69a4f4496abbd651f3dee55033a28b8ca5a91fe2
github username <github email address>
2018-09-15 15:04:46 -0600

    added name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :recipes
+  validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
