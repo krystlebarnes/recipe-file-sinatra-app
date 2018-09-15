896e0c8f498b7ff696dde8d9602d20d7b680dbc5
github username <github email address>
2018-09-15 17:14:43 -0600

    add post route for new recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,4 +17,11 @@ class RecipesController < ApplicationController
     end
   end
 
+  post '/recipes' do
+    @recipe = Recipe.new(:name => params[:name], :description => params[:description], :serving_size => params[:serving_size], :prep_time => params[:prep_time], :cook_time => params[:cook_time], :ingredients , :name => params[:name]=> params[:ingredients], :instructions => params[:instructions])
+    @recipe.user_id = current_user.id
+    @recipe.save
+    redirect "/recipes/#{@recipe.slug}"
+  end
+
 end
