115619bdd7a8f3767ea9b779791dc11e394f3277
github username <github email address>
2018-09-15 12:59:21 -0600

    added post route for signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,12 @@ class UsersController < ApplicationController
       redirect "/recipes"
     end
   end
+
+  post "/signup" do
+    user = User.new(email: params[:email], password: params[:password])
+    user.save
+    session[:user_id] = user.id
+    redirect to "/recipes"
+  end
+
 end
