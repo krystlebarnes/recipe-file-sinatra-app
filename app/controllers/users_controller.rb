2786a818c4d3b0eee3541cc2cd60c892147bcd07
github username <github email address>
2018-09-19 10:48:31 -0600

    added validation-specific error messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,34 @@ class UsersController < ApplicationController
 
   post "/signup" do
     user = User.new(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    if !user.save
-      flash[:message] = "Oops, something went wrong. Please make sure all fields are filled out and the password is at least 8 characters long."
-      redirect "/signup"
-    else
+    if user.save
       user.save
       session[:user_id] = user.id
       redirect "/recipes"
+    else
+      case
+      when params[:name].empty?
+        flash[:message] = "Oops, we need to know your name."
+        redirect "/signup"
+      when params[:email].match(/\A[^@]+@([^@\.]+\.)+[^@\.]+\z/) == nil
+        flash[:message] = "Oops, we need to know your email."
+        redirect "/signup"
+      when User.find_by(email: params[:email])
+        flash[:message] = "Oops, there's already an account with that email."
+        redirect "/signup"
+      when params[:password].empty?
+        flash[:message] = "Oops, you need a password."
+        redirect "/signup"
+      when params[:password].size < 8
+        flash[:message] = "Oops, your password needs to be at least 8 characters long."
+        redirect "/signup"
+      when params[:password] != params[:password_confirmation]
+        flash[:message] = "Oops, your passwords didn't match."
+        redirect "/signup"
+      else
+        flash[:message] = "Oops, something went wrong. Let's try that again."
+        redirect "/signup"
+      end
     end
   end
 
@@ -42,6 +63,12 @@ class UsersController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/recipes"
+    elsif !user
+      flash[:message] = "Oops, we couldn't find an account with that email."
+      redirect "/login"
+    elsif !user.authenticate(params[:password])
+      flash[:message] = "Oops, that password is incorrect."
+      redirect "/login"
     else
       flash[:message] = "Oops, something went wrong. Let's try that again."
       redirect "/login"
