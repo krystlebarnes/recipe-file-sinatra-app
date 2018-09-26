f87f62066c9c743b96824cf2a391d504f9aed5ff
github username <github email address>
2018-09-26 17:47:46 -0600

    added user_id validation for Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,5 @@ class Recipe < ActiveRecord::Base
   validates :cook_time, numericality: { only_integer: true }, allow_nil: true
   validates :ingredients, presence: true
   validates :instructions, presence: true
+  validates :user_id, presence: true
 end
